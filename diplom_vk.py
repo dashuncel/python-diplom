f834a43c19ec6d17237fbce716532628076ec58a
dashuncel <korovkinada@ya.ru>
2018-10-16 03:53:53 +0800

    Add files via upload

--- diplom_vk.py
+++ diplom_vk.py
@@ -1,119 +1,105 @@
+from urllib.parse import urlencode
 import requests
-import progressbar
 import time
 import json
+import progressbar
 import sys
 
-#TOKEN = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
-#USER_ID = '171691064'
-
-TOKEN = '19da8c6dce232885ee0d47afd2d0773f0d7e4bfad13fe11dd7c6c7c567ce60458b9fd04ebba685be4ea10'
+TOKEN = 'cbe61b1e76142e4ed58ae1c9461b3a794f384b79d36890ca0cc7c66826dcbcb01a2e5027898d97c1f7d0a'
 USER_ID = '9297520'
-
-class UserVk:
-
-    def __init__(self, user_id, token):
-        self.ERROR = 'error_msg'
-        self.id = user_id
-        self.params = {'access_token': token,
-                       'v': '5.85'}
-
-    def __query_vk__(self, items, url):
-        # произвольный vk-запрос url с параметрами items
-        #print(url.format(*items))
-        response = None
-        while response is None:
-            try:
-                response = requests.get(url.format(*items), self.params).json()
-            except Exception as e:
-                print('Ошибка обращения к API VK:', e)
-                time.sleep(5)
-
-        if response.get('error'):
-            return response['error']
-        elif response.get('response'):
-            return response['response']
-        else:
-            return ''
-
-    def get_friends(self, user_id, user_info):
-        # список друзей для user_id
-        url = 'https://api.vk.com/method/friends.get?user_id={}&fields={}'
-        friend_list = self.__query_vk__((user_id, user_info), url)
-        if friend_list.get('items'):
-            return friend_list['items']
-        elif friend_list.get(self.ERROR):
-            return friend_list[self.ERROR]
-        else:
-            return friend_list
-
-    def get_groups(self, user_id, group_info='', type_res='set'):
-        # возвращает список групп для user_id
-        url = 'https://api.vk.com/method/groups.get?user_id={}&extended=1&filter=groups&fields={}'
-        group_list = self.__query_vk__((user_id, group_info), url)
-        if group_list.get('items'):
-            if type_res == 'set':
-                my = set(grp['id'] for grp in group_list['items'])
-                return set(my)
-            else:
-                return group_list['items']
-        elif group_list.get(self.ERROR):
-            return group_list[self.ERROR]
-        else:
-            return list()
-
-    def get_users_info(self, target_ids, info):
-        # target_ids - список, айдишники пользователей
-        # info - строка, допинформация через запятую, которую надо получить для каждого id
-        target_ids = ','.join(str(x) for x in target_ids)
-        url = 'https://api.vk.com/method/users.get?user_ids={}&fields={}'
-        user_info = self.__query_vk__((target_ids, info), url)
-        return user_info
-
-    @property
-    def user_group(self):
-        return self.get_groups(self.id, 'name,members_count', 'list')
-
-    @property
-    def user_friends(self):
-        return self.get_friends(self.id, '')
+PARAMS = {'access_token': TOKEN,
+          'v': '5.85'}
 
 
-main_user = UserVk(USER_ID, TOKEN)
-main_groups = main_user.user_group
-main_friends = main_user.user_friends
+def query_vk(url):
+    response = None
+    while response is None:
+        try:
+            response = requests.get(url, PARAMS).json()
+        except Exception as e:
+            print('Ошибка обращения к API VK:', e)
+            time.sleep(5)
+    return response
 
-if not type(main_groups) is set and not type(main_groups) is list:
-    print(f'Не могу получить список групп главного пользователя. Ошибка:{main_groups}')
-    exit(0)
 
-friends_groups = set()
-bar = progressbar.ProgressBar(maxval=len(main_friends),
-                              fd=sys.stdout,
-                              widgets=['Информация о друзьях',
-                                       progressbar.Bar(left='[', marker='-',right=']'),progressbar.Percentage()])\
-                 .start()
-for idx, friend in enumerate(main_friends):
-    bar.update(idx)
-    group_set = main_user.get_groups(friend)
-    if type(group_set) is set:
-        friends_groups.update(group_set)
-bar.finish()
+def get_url(method, url_params):
+    url = '?'.join((f'https://api.vk.com/method/{method}', urlencode(url_params)))
+    return url
 
-if not main_groups is set:
-    main_groups_set = set(grp['id'] for grp in main_groups)
-else:
-    main_groups_set = main_groups
 
-unique_groups = main_groups_set.difference(friends_groups)
-unique_groups_info = [{'name': group['name'], 'gid': group['id'], 'members_count': group['members_count']}
-                    for group in main_groups if group['id'] in unique_groups]
-print(f'Группы, в которых состоит только пользователь {main_user.id}\n', unique_groups_info)
+def get_result(res, node_name='response', err_name='error'):
+    if res.get(node_name):
+        return res[node_name]
+    elif res.get(err_name):
+        return res[err_name]
+    else:
+        return res
 
-with open('groups.json', 'w', encoding='utf-8') as f:
-    try:
-        json.dump(unique_groups_info, f, ensure_ascii=False, indent=4)
-    except Exception as e:
-        print(f'Ошибка записи в файл {e}')
+
+def get_friends(user_id, user_info=''):
+    url = get_url('friends.get', {'user_id': user_id,
+                                  'fields': user_info})
+    friend_list = get_result(get_result(query_vk(url)), 'items', 'error_msg')
+    return friend_list
+
+
+def get_groups(user_id, fields=''):
+    url = get_url('groups.get', {'user_id': user_id,
+                                 'extended': 1,
+                                 'filter': 'groups',
+                                 'fields': fields})
+    group_list = get_result(get_result(query_vk(url)), 'items', 'error_msg')
+    return group_list
+
+
+def put_in_file(file_name, obj):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        try:
+            json.dump(obj, f, ensure_ascii=False, indent=4)
+        except Exception as e:
+            print(f'Ошибка записи в файл {e}')
+
+
+def set_group_id(group):
+    if type(group) is list:
+        groups_set = set(grp['id'] for grp in group)
+    else:
+        groups_set = set()
+    return groups_set
+
+
+def compare_groups(user_groups, friend_list):
+    user_groups_set = set_group_id(user_groups)
+
+    bar = progressbar.ProgressBar(maxval=len(friend_list),
+                                  fd=sys.stdout,
+                                  widgets=['Информация о друзьях',
+                                           progressbar.Bar(left='[', marker='-', right=']'), progressbar.Percentage()])\
+                     .start()
+    print('Получение списка групп, в которых состоят друзья')
+    friends_groups = set()
+    for idx, friend in enumerate(friend_list):
+        bar.update(idx)
+        friends_groups.update(set_group_id(get_groups(friend)))
+    bar.finish()
+
+    unique_groups = user_groups_set.difference(friends_groups)
+    unique_groups_info = [{'name': group['name'], 'gid': group['id'], 'members_count': group['members_count']}\
+                          for group in user_groups if group['id'] in unique_groups]
+    return unique_groups_info
+
+
+if __name__ == '__main__':
+
+    user_groups = get_groups(USER_ID, 'members_count')
+
+    if type(user_groups) is str:
+        print(f'Не могу получить список групп главного пользователя. Ошибка:{user_groups}')
+        exit(0)
+
+    user_friends = get_friends(USER_ID)
+
+    put_in_file('groups.json', compare_groups(user_groups, user_friends))
+
 
 
